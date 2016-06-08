e2489c270d6ec232cbbf8c97d76e2c1e138a9e37
Kate <irakata@yandex.ru>
2016-06-08 22:30:44 +0700

    Signed-off-by: Kate <irakata@yandex.ru>

--- mainwindow.cpp
+++ mainwindow.cpp
@@ -9,19 +9,6 @@ MainWindow::MainWindow(QWidget *parent) :
     ui(new Ui::MainWindow)
 {
     ui->setupUi(this);
-    ui->production_BTN->setStyleSheet("QPushButton{color: #fff; "
-                                                  "text-decoration: none; "
-                                                  "user-select: none; "
-                                                  "background: rgb(0, 128, 0); /* фон кнопки */"
-                                                  "padding: .7em 1.5em; }"
-                                      "QPushButton:hover{background: rgb(34, 139, 34); border:none;}");
-
-    ui->supply_BTN->setStyleSheet("QPushButton{color: #fff; "
-                                             "text-decoration: none; "
-                                             "user-select: none; "
-                                             "background: rgb(0, 128, 0); /* фон кнопки */"
-                                             "padding: .7em 1.5em; }"
-                                  "QPushButton:hover{background: rgb(34, 139, 34); border:none;}");
 }
 
 MainWindow::~MainWindow()
